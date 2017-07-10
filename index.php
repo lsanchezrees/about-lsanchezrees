a4fe07aaa96a9ef27bb585dbee3f2a7963510b53
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-10 16:12:59 -0600

    added more text to personal information

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		several years.  My next incarnation was as an acupuncturist and D.O.M.  I then met my husband, got married, and...
 		returned to New Mexico, which has more acupuncturists per capita then Beijing.  I have just recently completed
 		a Certificate in Web Design, which involved a lot of HTML and CSS and a little JavaScript and PHP.</p>
-		<p>I am married to a wonderful man named Brian.  My hobbies are hiking and reading, and I have a dog, Lucy, who is incredibly sweet and adorable.  She is a shy
+		<p>I am married to a wonderful man named Brian.  He works for LANL.  My hobbies are hiking and reading, and I have a dog, Lucy, who is incredibly sweet and adorable.  She is a shy
 			little girl who loves to hunt lizards and bugs and loves to eat bees.  (I'm not kidding about that last
 		thing.)  I also love to travel and have been to 5 continents.  For our honeymoon, my husband and I hiked the
 		Inca Trail to Machu Picchu.</p>
