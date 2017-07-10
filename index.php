c662a788e0f5bd3fd76aa0a43e10d3514bdc5cfc
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-10 16:47:06 -0600

    updated and added the css sheet

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="utf-8">
 		<title>About Leonora</title>
+		<link href="css/styles.css" rel="stylesheet" type="text/css" />
+
 	</head>
 	<body>
 		<h1>About Leonora Sanchez-Rees</h1>
@@ -11,13 +13,17 @@
 		several years.  My next incarnation was as an acupuncturist and D.O.M.  I then met my husband, got married, and...
 		returned to New Mexico, which has more acupuncturists per capita then Beijing.  I have just recently completed
 		a Certificate in Web Design, which involved a lot of HTML and CSS and a little JavaScript and PHP.</p>
-		<p>I am married to a wonderful man named Brian.  He works for LANL.  My hobbies are hiking and reading, and I have a dog, Lucy, who is incredibly sweet and adorable.  She is a shy
+		<p>I am married to a great guy named Brian.  He works for LANL.  My hobbies are hiking and reading, and I have
+			a dog, Lucy, who is incredibly sweet and adorable.  She is a shy
 			little girl who loves to hunt lizards and bugs and loves to eat bees.  (I'm not kidding about that last
 		thing.)  I also love to travel and have been to 5 continents.  For our honeymoon, my husband and I hiked the
 		Inca Trail to Machu Picchu.</p>
-		<p>Since I have no pictures of myself on this laptop, I'll have to substitute a picture of Lucy, my
-		pointer mix, and Bailey, my beloved Katrina rescue who died last year.</p>
-		<img src="images/baileylucy.jpg" alt="Bailey and Lucy" width="400px" height="auto">
+		<p>The picture below is the only picture of myself I could find on my phone.  It's a picture of my husband, Brian
+		and I at Ayers Rock, Australia.  We're taking a camel ride to a dinner under the stars.</p>
+		<p>The picture next to it is a picture of my dogs, Bailey and Lucy.  Bailey was a Katrina rescue who died last
+		year.  We got Lucy in December 2014. </p>
 
+		<img src="images/australia.png" alt="Australia" >
+		<img src="images/baileylucy.jpg" alt="Bailey & Lucy" >
 	</body>
 </html>
