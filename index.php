c04a47b0c015a14222f32d4b65debcbf662371c2
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-10 16:11:47 -0600

    created file about-lsanchezrees

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>About Leonora</title>
+	</head>
+	<body>
+		<h1>About Leonora Sanchez-Rees</h1>
+		<p>I have had several careers so far.  I have a BA in Anthropology, and an MA in Linguistics.
+			I then got an AAS in Information Systems and worked as a programmer and systems analyst for
+		several years.  My next incarnation was as an acupuncturist and D.O.M.  I then met my husband, got married, and...
+		returned to New Mexico, which has more acupuncturists per capita then Beijing.  I have just recently completed
+		a Certificate in Web Design, which involved a lot of HTML and CSS and a little JavaScript and PHP.</p>
+		<p>I am married to a wonderful man named Brian.  My hobbies are hiking and reading, and I have a dog, Lucy, who is incredibly sweet and adorable.  She is a shy
+			little girl who loves to hunt lizards and bugs and loves to eat bees.  (I'm not kidding about that last
+		thing.)  I also love to travel and have been to 5 continents.  For our honeymoon, my husband and I hiked the
+		Inca Trail to Machu Picchu.</p>
+		<p>Since I have no pictures of myself on this laptop, I'll have to substitute a picture of Lucy, my
+		pointer mix, and Bailey, my beloved Katrina rescue who died last year.</p>
+		<img src="images/baileylucy.jpg" alt="Bailey and Lucy" width="400px" height="auto">
+
+	</body>
+</html>
